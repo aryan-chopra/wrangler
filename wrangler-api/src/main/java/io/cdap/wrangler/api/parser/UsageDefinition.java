66d58b149ae1a4e2c1debe94e7eb87bb82d06658
aryan-chopra <aryan.chopra1211@gmail.com>
2025-04-11 02:56:03 +0530

    Updated UsageDefinition.java to support BYTE_SIZE and TIME_DURATION

--- wrangler-api/src/main/java/io/cdap/wrangler/api/parser/UsageDefinition.java
+++ wrangler-api/src/main/java/io/cdap/wrangler/api/parser/UsageDefinition.java
@@ -16,11 +16,11 @@
 
 package io.cdap.wrangler.api.parser;
 
-import io.cdap.wrangler.api.Optional;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
+import io.cdap.wrangler.api.Optional;
 
 /**
  * This class {@link UsageDefinition} provides a way for users to registers the argument for UDDs.
@@ -126,6 +126,10 @@ public final class UsageDefinition implements Serializable {
           sb.append("prop:{key:value,[key:value]*");
         } else if (token.type().equals(TokenType.RANGES)) {
           sb.append("start:end=[bool|text|numeric][,start:end=[bool|text|numeric]*");
+        } else if (token.type().equals(TokenType.BYTE_SIZE)) {
+          sb.append(token.name());
+        } else if (token.type().equals(TokenType.TIME_DURATION)) {
+          sb.append(token.name());
         }
       }
 
